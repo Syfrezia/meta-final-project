bfb4f8351e09f5571339f8b5ab5bed7bd82a1820
syfrezia <syafrie.mufariza98@gmail.com>
2023-12-30 01:22:15 +0700

    edit: accept disabled and className props to PrimaryButton

--- src/components/common/PrimaryButton.jsx
+++ src/components/common/PrimaryButton.jsx
@@ -1,11 +1,19 @@
 import { Button } from "react-bootstrap";
 
-const PrimaryButton = ({ as, to, type, children }) => (
+const PrimaryButton = ({
+  as,
+  to,
+  type,
+  disabled,
+  className = "",
+  children,
+}) => (
   <Button
     as={as}
     to={to}
     type={type}
-    className="mt-3 px-4 py-2 font-karla rounded-4 bg-pri-yellow border border-2 border-white text-black fw-bolder"
+    disabled={disabled}
+    className={`px-4 py-2 font-karla rounded-4 bg-pri-yellow border border-2 border-white text-black fw-bolder ${className}`}
   >
     {children}
   </Button>
